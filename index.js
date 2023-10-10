2c20f68c6bac67bb842ae39ffab24b49d73e253c
CECIL7845 <odonkorcecil123@gmail.com>
2023-10-10 22:36:25 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const player = "Cecil"
+const opponent = "Cedric"
+const game = "AmazingFighter"
+let points = 0
+let hasWon = false
+
+points += 100
+hasWon = true
+
+if (hasWon) {
+    console.log(`${player} got ${points} points and won the ${game} game!`)
+} else {
+    console.log(`The winner is ${opponent} !  ${player} lost the game`)
+}
